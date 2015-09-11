4f3c600f1167dcec767bb477123e95bae8d41b3a
comp1 <comp1@comp1_01.lab.unesc.net>
2015-09-11 20:02:52 -0300

    Definido o funcionario par cálculo da folha

--- BuscaArquivos/src/ExemploA.java
+++ BuscaArquivos/src/ExemploA.java
@@ -5,9 +5,8 @@
 public class ExemploA {
     //Primeiro exemplo de acesso ao git
  
-   public double calcularFolha(){
-       //TODO: Implementar
-       return 0;
+   public double calcularFolha(Funcionario funcionario){
+       return funcionario.getSalario() * 2;
    }
     
 }
